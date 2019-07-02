50a7f95317ec5920c677176071e5b1a20fc64bb8
Christa Stephens <christastephens@quickenloans.com>
2019-07-02 08:35:52 -0400

    changed to interpolation

--- Program.cs
+++ Program.cs
@@ -15,6 +15,8 @@ namespace NumberGuesser
             string appAuthor = "Christa Stephens";
 
             Console.WriteLine("{0}: Version {1} by {2}", appName, appVersion, appAuthor);
+            //easier verion of interpolation so that you don't have to keep track of var order
+            Console.WriteLine($"{appName}: Version {appVersion} by {appAuthor}");
 
         }
     }
